368b2d98eeed6930a6c3e832ffbf70a549460195
Mads Nielsen <post@madsnielsen.dk>
2018-09-23 14:16:46 +0200

    first working version

--- DataEnum/Program.cs
+++ DataEnum/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Antlr4.StringTemplate;
@@ -14,67 +12,262 @@ namespace DataEnum
     {
         static void Main(string[] args)
         {
-            var tds = TableDefinition.FromFile(@"table-def01.json");
+            var tds = TableDefinition.FromFile(@"table-def02.json");
+            var stg = new TemplateGroupFile(new FileInfo(@"table-def.stg").FullName);
             foreach (var td in tds)
             {
                 Console.WriteLine($"Table {td.name}");
                 foreach (
                     (string name, string fullname)
                     in td.members
-                        .SelectMany(m => m.GetFullNames()
-                        .Select(fn => (m.name, fn))))
+                        .SelectMany(m => m.GetMembersPrimitive()
+                        .Select(mp => (name: m.name, fullname: mp.fullname))))
                 {
                     Console.WriteLine($"{name}: {fullname}");
                 }
+                Console.WriteLine("==========");
+                var ts = stg.GetInstanceOf("tablescript");
+                ts.Add("td", td);
+                Console.WriteLine(ts.Render());
             }
+            Stack<string> test = new Stack<string>();
+            test.Push("a"); test.Push("b"); test.Push("c");
+            Console.WriteLine(string.Join("", test.Reverse()));
             while (Console.ReadKey().Key != ConsoleKey.Spacebar);
         }
     }
 
     class TableDefinition
     {
+        public IList<MemberDefinition> memberDefinitions = new List<MemberDefinition>();
         public IList<Member> members = new List<Member>();
+        public string memberSeparator = ".";
         public string name;
         public ICollection<DataType> dataTypes = new List<DataType>() {
-            new DataType() { name = "num", primitive = true },
-            new DataType() { name = "str", primitive = true }
+            DataType.typeSig,
+            new DataType() { name = "num" },
+            new DataType() { name = "str" },
+            new DataType() { name = "int" },
+            new DataType() { name = "varchar(max)" }
         };
 
-        public class DataType
+        public class AlternativeDefinition
         {
-            public IList<Alternative> alternatives;
             public string name;
-            public bool primitive = false;
+            public IList<MemberDefinition> fieldDefinitions;
 
-            public class Alternative
+            public Alternative AsAlternative(int index, Context context)
             {
-                public string name;
-                public IList<Member> fields;
-            }
+                var alternative = new Alternative()
+                {
+                    name = name,
+                    fullname = context.GetFullName(name),
+                    fieldDefinitions = fieldDefinitions
+                };
 
-            public List<string> GetFullNames()
-            {
-                if (primitive)
+                context.fullnamePrefixes.Push(name);
                 {
-                    return new List<string>() { name };
+                    alternative.index = index;
+                    alternative.fields = alternative.fieldDefinitions.Select(fd => fd.AsMember(context)).ToList();
                 }
-                else
-                {
-                    return alternatives.SelectMany(a => a.fields.SelectMany(f => f.GetFullNames())).ToList();
-                }
+                context.fullnamePrefixes.Pop();
+
+                return alternative;
             }
         }
 
-        public class Member
+        public class Alternative : AlternativeDefinition
+        {
+            public IList<Member> fields;
+            public IList<MemberPrimitive> fieldsOther;
+            public string fullname;
+            public int index;
+
+            public IList<MemberPrimitive> GetMembersPrimitive()
+            {
+                return fields.SelectMany(f => f.GetMembersPrimitive()).ToList();
+            }
+        }
+
+        public class Context
+        {
+            public string FullnamePrefix {
+                get {
+                    return string.Join(memberSeparator, fullnamePrefixes.Reverse());
+                }
+            }
+            public Stack<string> fullnamePrefixes = new Stack<string>();
+            public string memberSeparator;
+
+            public string GetFullName(string name)
+            {
+                return FullnamePrefix == "" ? name : FullnamePrefix + memberSeparator + name;
+            }
+        }
+
+        public class DataType
+        {
+            // TODO: add to syntax
+            public bool addSignal = true;
+            private IList<AlternativeDefinition> alternatives = new List<AlternativeDefinition>();
+            public IList<AlternativeDefinition> Alternatives
+            {
+                get
+                {
+                    return alternatives;
+                }
+                set
+                {
+                    alternatives = value;
+                    if (value != null)
+                    {
+                        Primitive = false; 
+                    }
+                }
+            }
+            public bool Multi { get { return Alternatives.Count > 1; } }
+            public string name;
+            private bool primitive = true;
+            public bool Primitive
+            {
+                get
+                {
+                    return primitive;
+                }
+                set {
+                    CheckValid(value, Alternatives);
+                    primitive = value;
+                }
+            }
+            /// <summary>Should signal fields be added?</summary>
+            public bool Signal { get { return Multi && addSignal; } }
+            public static readonly DataType typeSig = new DataType() { name = "sig" };
+
+            /// <summary>
+            /// Assumes a particular combination of 'primitive' and 'alternatives' values is valid. If not, an exception is thrown.
+            /// </summary>
+            /// <exception cref="ArgumentException">If the combination is not valid</exception>
+            /// <param name="primitive"></param>
+            /// <param name="alternatives"></param>
+            private static void CheckValid(bool primitive, object alternatives)
+            {
+                if (!((primitive && alternatives == null) || (!primitive && alternatives != null)))
+                {
+                    throw new ArgumentException("Data type must be either a primitive with no alternatives or a non-primitive with at least one alternative.");
+                }
+            }
+
+            public IList<Alternative> GetAlternatives(Context context)
+            {
+                IList<Alternative> alternatives = new List<Alternative>();
+                // populate all alternative with the own member instances
+                for (var index = 0; index < Alternatives.Count; index++)
+                {
+                    alternatives.Add(Alternatives[index].AsAlternative(index, context));
+                }
+
+                // populate all alternatives with a list of member instances in the other alternatives
+                foreach (var alternative in alternatives)
+                {
+                    alternative.fieldsOther = alternatives
+                        .Where(a => a != alternative)
+                        .SelectMany(ao => ao.GetMembersPrimitive())
+                        .ToList();
+                }
+
+                return alternatives;
+            }
+
+            public string GetSignalName(Context context)
+            {
+                return name + context.memberSeparator + "s";
+            }
+        }
+
+        public class MemberDefinition
         {
             public DataType dataType;
             public string dataTypeName;
             public string name;
 
-            public List<string> GetFullNames()
+            /// <summary>
+            /// Returns the member definition as a Member instance
+            /// </summary>
+            /// <param name="fullnamePrefix"></param>
+            /// <returns></returns>
+            public Member AsMember(Context context)
             {
-                return dataType.GetFullNames().Select(n => name + '.' + n).ToList();
+                string fullname = context.GetFullName(name);
+                context.fullnamePrefixes.Push(name);
+                Member member = new Member()
+                {
+                    dataType = dataType,
+                    name = name,
+                    fullname = fullname,
+                    alternatives = dataType.GetAlternatives(context),
+                    signal = dataType.Signal,
+                    signalFullname = context.GetFullName(dataType.GetSignalName(context))
+                };
+                context.fullnamePrefixes.Pop();
+                return member;
             }
+
+            public MemberPrimitive AsMemberPrimitive()
+            {
+                return new MemberPrimitive()
+                {
+                    dataType = dataType,
+                    name = name
+                };
+            }
+
+            public MemberPrimitive AsMemberPrimitive(Context context)
+            {
+                var member = AsMemberPrimitive();
+                member.fullname = context.GetFullName(name);
+                return member;
+            }
+        }
+
+        /// <summary>
+        /// Instance of an abstract member, which may have underlying alternatives and primitive members
+        /// </summary>
+        public class Member : MemberDefinition
+        {
+            public IList<Alternative> alternatives = new List<Alternative>();
+            public string fullname;
+            public bool signal = false;
+            public string signalFullname;
+
+            public new MemberPrimitive AsMemberPrimitive()
+            {
+                var member = (this as MemberDefinition).AsMemberPrimitive();
+                member.fullname = fullname;
+                return member;
+            }
+
+            /// <summary>
+            /// Returns the member itself and all submembers. Full names prefixed with provided value.
+            /// </summary>
+            public IList<MemberPrimitive> GetMembersPrimitive()
+            {
+                if (dataType.Primitive)
+                {
+                    return new List<MemberPrimitive>() { AsMemberPrimitive() };
+                }
+                else
+                {
+                    return alternatives.SelectMany(a => a.GetMembersPrimitive()).ToList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instance of a primitive member
+        /// </summary>
+        public class MemberPrimitive : MemberDefinition
+        {
+            public string fullname;
         }
 
         public static ICollection<TableDefinition> FromFile(string path)
@@ -86,8 +279,10 @@ namespace DataEnum
             var obj = ser.Deserialize(reader) as JObject;
             foreach (var prop in obj.Properties())
             {
-                var table = new TableDefinition();
-                table.name = prop.Name;
+                var table = new TableDefinition
+                {
+                    name = prop.Name
+                };
                 var defObj = prop.Value as JObject;
                 foreach (var defProp in defObj.Properties())
                 {
@@ -97,18 +292,27 @@ namespace DataEnum
                             var typesObj = defProp.Value as JObject;
                             foreach (var typeProp in typesObj.Properties())
                             {
-                                var type = new DataType();
-                                type.name = typeProp.Name;
-                                type.alternatives = new List<DataType.Alternative>();
+                                var type = new DataType
+                                {
+                                    name = typeProp.Name,
+                                    Alternatives = new List<AlternativeDefinition>()
+                                };
                                 var altsObj = typeProp.Value as JObject;
                                 foreach (var altProp in altsObj.Properties())
                                 {
-                                    var alt = new DataType.Alternative();
-                                    alt.name = altProp.Name;
-                                    alt.fields = (altProp.Value as JArray)
-                                        .Select(elem => new Member() { dataTypeName = (elem as JValue).Value as string })
-                                        .ToList();
-                                    type.alternatives.Add(alt);
+                                    var alt = new AlternativeDefinition
+                                    {
+                                        name = altProp.Name,
+                                        fieldDefinitions = (altProp.Value as JObject)
+                                            .Properties()
+                                            .Select(elem => new MemberDefinition()
+                                            {
+                                                name = elem.Name,
+                                                dataTypeName = (elem.Value as JValue).Value as string
+                                            })
+                                            .ToList()
+                                    };
+                                    type.Alternatives.Add(alt);
                                 }
                                 table.dataTypes.Add(type);
                             }
@@ -117,10 +321,26 @@ namespace DataEnum
                             var memsObj = defProp.Value as JObject;
                             foreach (var memProp in memsObj.Properties())
                             {
-                                var mem = new Member();
-                                mem.name = memProp.Name;
-                                mem.dataTypeName = (memProp.Value as JValue).Value as string;
-                                table.members.Add(mem);
+                                var mem = new MemberDefinition
+                                {
+                                    name = memProp.Name,
+                                    dataTypeName = (memProp.Value as JValue).Value as string
+                                };
+                                table.memberDefinitions.Add(mem);
+                            }
+                            break;
+                        case "settings":
+                            var setObj = defProp.Value as JObject;
+                            foreach (var setProp in setObj.Properties())
+                            {
+                                switch (setProp.Name)
+                                {
+                                    case "member-separator":
+                                        table.memberSeparator = (setProp.Value as JValue).Value as string;
+                                        break;
+                                    default:
+                                        throw new NotSupportedException($"Unknown setting: {setProp.Name}");
+                                }
                             }
                             break;
                         default:
@@ -128,20 +348,19 @@ namespace DataEnum
                     }
                 }
 
-                foreach (var dataType in table.dataTypes.Where(dt => !dt.primitive))
+                foreach (var dataType in table.dataTypes.Where(dt => !dt.Primitive))
                 {
-                    foreach (var alt in dataType.alternatives)
+                    foreach (var alt in dataType.Alternatives)
                     {
-                        for (var index = 0; index < alt.fields.Count; index++)
+                        for (var index = 0; index < alt.fieldDefinitions.Count; index++)
                         {
-                            Member field = alt.fields[index];
+                            MemberDefinition field = alt.fieldDefinitions[index];
                             field.dataType = table.dataTypes.Single(dt => dt.name == field.dataTypeName);
-                            field.name = alt.name + ':' + index.ToString();
                         }
                     }
                 }
 
-                foreach (var member in table.members)
+                foreach (var member in table.memberDefinitions)
                 {
                     try
                     {
@@ -154,10 +373,21 @@ namespace DataEnum
                     }
                 }
 
+                // create member instances from definitions
+                Context context = table.GetContext();
+                table.members = table.memberDefinitions
+                    .Select(md => md.AsMember(context))
+                    .ToList();
+
                 tables.Add(table);
             }
 
             return tables;
+        }
+
+        public Context GetContext()
+        {
+            return new Context() { memberSeparator = memberSeparator };
         }
     }
 }
