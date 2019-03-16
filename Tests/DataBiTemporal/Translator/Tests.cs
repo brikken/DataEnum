58c26c5177367e638091f13d5d43fdfb3200d064
Mads Nielsen <post@madsnielsen.dk>
2019-03-16 17:37:07 +0100

    translator test (unfinished)

---
+++ Tests/DataBiTemporal/Translator/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBiTemporal.Translators;
+using System.IO;
+
+namespace Tests.DataBiTemporal.Translator
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            var fiSql = new FileInfo("basic.sql");
+            var defs = BiTemporal.GetDefinitions(File.ReadAllText(fiSql.FullName));
+            Assert.AreEqual(1, defs.Count);
+            var def = defs.GetEnumerator().Current;
+            Assert.AreEqual("bitemp", def.Options.);
+        }
+    }
+}
